d621e38c763abe67c85beedbd954a53bf2ad6e65
Gore Auluck <gore.auluck@gmail.com>
2024-03-04 17:08:19 -0800

    one last test for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Hero from './pages/Hero';
 
 function App() {
   return (
-    <div className='h-dvh w-full bg-black font-sans cursor-potato'>
+    <div className='min-h-dvh w-full bg-black font-sans cursor-potato'>
       <Hero />
     </div>
   );
