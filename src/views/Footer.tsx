a543f20276c51b8e859229627e37485383564618
Gore Auluck <gore.auluck@gmail.com>
2024-04-07 12:47:31 -0700

    adjusting the gap on contact section and increasing footer height

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className='w-full h-12 font-monospace text-silver text-center p-8 flex items-center justify-center'>
+    <div className='w-full h-16 font-monospace text-silver text-center p-8 flex items-center justify-center'>
       <h1>Designed & Built by Gore Auluck</h1>
     </div>
   );
