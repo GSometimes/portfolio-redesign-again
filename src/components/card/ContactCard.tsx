2fa005e686a829f05db6ed93cd009cdf73403e7d
Gore Auluck <gore.auluck@gmail.com>
2024-04-06 20:53:00 -0700

    updated contact links and data

--- src/components/card/ContactCard.tsx
+++ src/components/card/ContactCard.tsx
@@ -1,24 +1,35 @@
+import { contactData } from '../../data/ContactData';
+import { motion } from 'framer-motion';
+
 const ContactCard = () => {
   return (
     <div className='w-full px-4 lg:px-8 my-16'>
-      <ul className='w-full rounded-3xl py-10 px-5 sm:py-20 sm:px-10 outline font-monospace flex items-center gap-4 hover:outline-orange-peel [&>*:last-child]:ml-auto text-sm sm:text-base '>
-        <li className='hover:text-orange-peel'>
-          <a href='https://www.linkedin.com/in/gore-auluck/'>LinkedIn</a>
-        </li>
-        <li className='hover:text-orange-peel'>
-          <a href='https://github.com/GSometimes'>Github</a>
-        </li>
-        <li className='hover:text-orange-peel'>
-          <a href='https://drive.google.com/file/d/1bg9jbPY4iZthCn5vxnpPWSb0c8EwtZe7P6l4onyCrV4/view'>
-            Resume
-          </a>
-        </li>
-        <li className='hover:text-orange-peel'>
-          <a href='mailto:gore.auluck@gmail.com'>gore.auluck@gmail.com</a>
-        </li>
-      </ul>
+      {/* <ul className='w-full rounded-3xl py-10 px-4 sm:py-20 sm:px-8 lg:px-14 outline font-monospace flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-8 lg:gap-12 hover:outline-orange-peel text-xl md:text-2xl lg:text-3xl text-light-green sm:text-silver  font-black tracking-wide'>
+        {contactData.map((contact, index) => (
+          <li key={index} className='hover:text-orange-peel'>
+            <a href={contact.link}>{contact.text}</a>
+          </li>
+        ))}
+      </ul> */}
+      <div className='grid justify-center grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6 font-monospace'>
+        {contactData.map((contact, index) => (
+          <motion.a
+            key={index}
+            href={contact.link}
+            target='_blank'
+            rel='noreferrer'
+            className='rounded-3xl py-10 px-4 sm:py-20 sm:px-8 lg:px-12 outline hover:outline-orange-peel hover:text-orange-peel'
+            whileHover={{ scale: 0.98 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            {contact.form}
+          </motion.a>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default ContactCard;
+
+// sm:[&>*:last-child]:ml-auto
---
+++ src/data/ContactData.tsx
@@ -0,0 +1,23 @@
+type Contact = {
+  form: string;
+  link: string;
+};
+
+export const contactData: Contact[] = [
+  {
+    form: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/gore-auluck/',
+  },
+  {
+    form: 'Resume',
+    link: 'https://drive.google.com/file/d/1bg9jbPY4iZthCn5vxnpPWSb0c8EwtZe7P6l4onyCrV4/view',
+  },
+  {
+    form: 'Github',
+    link: 'https://github.com/GSometimes',
+  },
+  {
+    form: 'Email',
+    link: 'mailto:gore.auluck@gmail.com',
+  },
+];
