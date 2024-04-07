1c06aa24a6cf8163c17a69dc5b754443988661d4
Gore Auluck <gore.auluck@gmail.com>
2024-04-06 23:18:05 -0700

    removed oopsie outline

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className='w-full font-monospace text-silver text-center outline p-2'>
+    <div className='w-full font-monospace text-silver text-center p-2'>
       <h1>Designed & Built by Gore Auluck</h1>
     </div>
   );
