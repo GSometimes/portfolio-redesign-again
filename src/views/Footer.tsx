5bb0920ee8f825bf321b64d04b049030b770da50
Gore Auluck <gore.auluck@gmail.com>
2024-04-17 00:51:14 -0700

    removed outline

--- src/views/Footer.tsx
+++ src/views/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className='w-full font-monospace text-silver text-center p-12 flex items-center justify-center outline'>
+    <div className='w-full font-monospace text-silver text-center p-12 flex items-center justify-center'>
       <h1>Designed & Built by Gore Auluck</h1>
     </div>
   );
