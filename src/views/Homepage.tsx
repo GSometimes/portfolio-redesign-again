6982c94dc9f0370347baa92665508075f5f22f0d
Gore Auluck <gore.auluck@gmail.com>
2024-04-17 19:54:12 -0700

    removed splash test

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -3,12 +3,10 @@ import About from './About';
 import Work from './Work';
 import Contact from './Contact';
 import Footer from './Footer';
-import Splash from './Splash';
 
 const Homepage = ({ startAnimation }: { startAnimation: boolean }) => {
   return (
     <div className='w-full bg-black'>
-      <Splash />
       <Hero startAnimation={startAnimation} />
       <About />
       <Work />
